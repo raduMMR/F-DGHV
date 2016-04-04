c825c7914d47d8a7fa987d340dde349ef6e200e1
radu mardari <radu.mardari12@gmail.com>
2016-04-04 23:06:36 +0300

    adaugat comentarii pentru functii

--- gsw.h
+++ gsw.h
@@ -94,22 +94,70 @@ vector<vector<ZZ> > BitDecomp(vector<vector<ZZ>> A, int l);
 
 
 /*
-
+@brief metoda BitDecomp_1 aplicata fiecarei linii a matricii A
+@param A matricea de intrare
+@param l = [log q]+1
+@return matricea de iesire cu liniile calculate dupa BitDecomp_1
 */
 vector<vector<ZZ>> BitDecomp_1(vector<vector<ZZ> > A, int l);
 
+/*
+@brief metoda Powersof2 aplicata vectorului b
+@param b vectorul de intrare b
+@param l = [log q] +1
+@return vectorul rezultat in urma transformarii Powersof2
+*/
 vector<vector<ZZ> > PowersOf2(vector<ZZ> b, int l);
 
+/*
+@brief Flatten = BitDecomp( BitDecomp_1 (C,l), l)
+*/
 vector<vector<ZZ> > Flatten(vector<vector<ZZ> > C, int l);
 
 // functii ajutatoare
+/*
+@brief genereaza o matrice n x m cu valori intregi in [0, q)
+*/
 vector<vector<ZZ> > GenerateMatrix(int n, int m, ZZ modulus_q);
+
+/*
+@brief inmultire a doua matrici
+TODO : imbunatatirea inmultirii cu metoda Coppersmith
+*/
 vector<vector<ZZ> > matrixMult(vector<vector<ZZ> > M1, vector<vector<ZZ> > M2);
+
+/*
+@brief adunare de matrici
+*/
 vector<vector<ZZ> > matrixAdd(vector<vector<ZZ> > M1, vector<vector<ZZ> > M2);
+
+/*
+@brief produsul scalar intre doi vectori
+*/
 ZZ dotProduct(vector<ZZ> v1, vector<ZZ> v2);
+
+/*
+@brief descompunerea in reprezentarea binara pe l biti a numarului a
+*/
 vector<ZZ> binary_representation(ZZ a, int l);
+
+/*
+@brief compunerea unui numar din reprezentarea binara in cea zecimala
+*/
 ZZ bin_to_ZZ(vector<ZZ> vec, int l);
+
+/*
+@brief reconstructia elementelor unui vector dintr-un vector expandat cu powerof2
+	din v = ( a_0, 2*a_0, ..., 2^(l-1)*a_0,..., a_(l-1), 2*a_(l-1), ..., 2^(l-1)*a_(l-1) )
+	in v = (a_0, a_1, ..., a_(l-1))
+
+*/
 vector<ZZ> powers_of_2_to_ZZ(vector<ZZ> powers, int l);
+
+/*
+@brief construirea unui vector de lungime l de forma 
+	v = ( elem, 2*elem, 2^2 * elem, ..., 2^(l-1)*elem)
+*/
 vector<ZZ> powers_of_2_from_ZZ(ZZ elem, int l);
 
 
